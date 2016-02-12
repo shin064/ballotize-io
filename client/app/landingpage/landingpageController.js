a624c557178a0de3ce5b24eca09ee7d56eafab74
Shin Adachi <hello@shinadachi.com>
2016-02-12 12:07:46 -0800

    [feat] implements client-side ballot creation form

--- client/app/landingpage/landingpageController.js
+++ client/app/landingpage/landingpageController.js
@@ -1,5 +1,7 @@
 var landingpage = angular.module('landingpage', []);
 
 landingpage.controller('landingpageController', ['$scope', function($scope){
+  
   this.hello = 'hello from landingpage controller';
+
 }]);
