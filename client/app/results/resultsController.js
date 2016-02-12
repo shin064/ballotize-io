3aa158863b9d0ad0482f178f420edf9e32d17638
Shin Adachi <hello@shinadachi.com>
2016-02-12 15:47:04 -0800

    [setup] sets up "room" factory

--- client/app/results/resultsController.js
+++ client/app/results/resultsController.js
@@ -1,5 +1,6 @@
 var results = angular.module('results', []);
 
-results.controller('resultsController', ['$scope', function($scope){
+results.controller('resultsController', ['$scope','room',function($scope,room){
   this.hello = 'hello from results controller!';
+  console.log('room inside results page: ',room);
 }]);
