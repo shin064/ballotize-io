e86359d6dd640c65c1dafebd0e4e1d74af152b8e
Shin Adachi <hello@shinadachi.com>
2016-02-13 14:48:42 -0800

    [feat] server handles new vote, emits "vote saved" socket event

--- server/functions/handleNewVote.js
+++ server/functions/handleNewVote.js
@@ -1,3 +1,23 @@
-module.exports = function(data){
-	console.log('handling new vote called');
+var db = require('../db.js');
+var io = require('../server.js');
+
+module.exports = function(data,socket){
+	console.log('handle new vote called');
+	console.log('data',data);
+
+	var roomcode = data.code;
+	var votername = data.username;
+	var choice = Number(data.choice);
+
+	db.Room.findOne({roomcode:roomcode},function(err,room){
+		room.voters[votername]=choice;
+		room.results[choice-1]++;
+		room.markModified('voters');
+		room.markModified('results');
+
+		room.save(function(err,room){
+			console.log('room saved: ',room);
+			socket.emit('vote saved', room);
+		})
+	})
 }
