954d70acd571e576f01a7894f6a3e3a47e47693a
Shin Adachi <hello@shinadachi.com>
2016-02-12 19:58:39 -0800

    [feat] server handles names (owners and voters)

--- server/db.js
+++ server/db.js
@@ -11,11 +11,12 @@ db.Schema = mongoose.Schema;
 db.roomSchema = new db.Schema ({
   roomcode: { type: Number, required: true},
   topic: { type: String },
+  owner: { type: String },
   options: {},
   numberOfWinners: { type: Number },
   voters: {},
   results: {}
-});
+}, {toJSON:{minimize:false}});
 //db.roomSchema.plugin(uniqueValidator);
 db.Room = mongoose.model('Room', db.roomSchema);
 
