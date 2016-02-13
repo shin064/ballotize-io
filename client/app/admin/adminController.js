18a99b2dadee4bcb26f9403f3383298a081dea98
Carl Bernardo <carlbernrdo@gmail.com>
2016-02-13 11:51:08 -0800

    [refactor] removes admin controller and view

--- client/app/admin/adminController.js
+++
@@ -1,7 +0,0 @@
-var admin = angular.module('admin', []);
-
-admin.controller('adminController', ['$scope', function($scope){
-  var ctrl = this;
-
-  ctrl.hello = 'hello from admin controller';
-}]);
