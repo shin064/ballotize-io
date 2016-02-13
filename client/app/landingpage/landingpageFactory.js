68dc9f77b426746a208a0c2d51cfb5e307bfb1b5
Carl Bernardo <carlbernrdo@gmail.com>
2016-02-12 17:03:00 -0800

    [feat] adds http req on submit of username/code

--- client/app/landingpage/landingpageFactory.js
+++ client/app/landingpage/landingpageFactory.js
@@ -1,6 +1,6 @@
 var landingpage = angular.module('landingpage');
 
-landingpage.factory('BallotizeFactory', function(){
+landingpage.factory('BallotizeFactory', ['$http', function($http){
   var code = '';
   var user = '';
 
@@ -20,10 +20,27 @@ landingpage.factory('BallotizeFactory', function(){
     return code;
   }
 
+  var getBallot = function(input, username){
+    $http({
+       method: 'GET',
+       url: '/ballot',
+       data: JSON.stringify({
+         code: input,
+         username: username
+       }),
+       headers: {'Content-Type': 'application/json'}
+     }).then(function success(response){
+       console.log('success',response)
+     }, function error(response){
+       console.log('error',response);
+     });
+  }
+
   return {
     setUser: setUser,
     getUser: getUser,
     setCode: setCode,
-    getCode: getCode
+    getCode: getCode,
+    getBallot: getBallot
   }
-});
+}]);
